64dd52dbbbd3b680a818512e0d4edd63d3019665
Diego Barajas (devsau) <81001205+DiegoBarajas@users.noreply.github.com>
2026-08-11 22:41:09 -0600

    Added Validator

--- templates/basic/src/routes/test.js
+++ templates/basic/src/routes/test.js
@@ -1,39 +1,50 @@
 const { Router } = require("@desaubv/quik");
-const { Status, Validator, ValidationException } = require("@desaubv/quik/http");
+const { Status, Validator, ValidationException, Types } = require("@desaubv/quik/http");
 const router = Router();
 
 router.get((req, res) => {
     res
         .status(Status.OK)
         .send("Test");
-})
+});
 
 router.post((req, res) => {
     function ageValidator(value) {
-        console.log("Entre al validator");
-        
-        if(value < 0) return undefined;
+        if (value < 0) throw new ValidationException("Age must be greater os equial than 0");
         return value;
-    }    
+    }
 
     const body = Validator.body(req.body, {
         "name": {
-            type: String,
+            type: [Types.Null, Types.String],
             required: true
         },
         "age": {
-            type: Number,
-            required: false,
+            type: Types.Int,
+            required: true,
             validator: ageValidator
         },
-        "lorem": {
-            type: [Number],
-
+        "status": {
+            type: Types.Array(Types.Int, Types.Boolean).canBeEmpty(),
+            required: true
+        },
+        "user": {
+            type: Types.Struct({
+                "name": {
+                    type: [Types.Null, Types.String],
+                    required: true
+                },
+                "age": {
+                    type: Types.Int,
+                    required: true,
+                    validator: ageValidator
+                }
+            }),
+            required: true
         }
-    })
+    });
 
     res.json(body);
-    
-})
+});
 
 module.exports = router;
